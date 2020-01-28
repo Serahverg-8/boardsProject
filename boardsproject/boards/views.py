15c7d12019f8434a34ff9b7a577fe5e3ff778df0
Varghese,Serah <serah.varghese@cerner.com>
2020-01-28 07:32:11 +0530

    Added models and home.html ...

--- boardsproject/boards/views.py
+++ boardsproject/boards/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Board
 # Create your views here.
-def home(request):
-     return HttpResponse('Hello, World!')
+def home_view(request):
+    boards = Board.objects.all()
+    return render(request, 'boards/home.html', {'boards': boards})
--- boardsproject/boardsproject/urls.py
+++ boardsproject/boardsproject/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from boards.views import home
+from boards.views import home_view
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',home),
-    path('home/',home),
+    path('',home_view),
+    path('home/',home_view),
 ]
